e7a6c96a93ea3339d8c97525cfd123a303c509b8
Manfred <manfred@manfred-debian8>
2018-04-01 15:07:01 -0430

    Prueba input con [(ngModel)]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/formulario/formulario.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario',
+  templateUrl: './formulario.component.html',
+  styleUrls: ['./formulario.component.css']
+})
+export class FormularioComponent implements OnInit {
+
+  public newCard: any = { text: ''};
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
